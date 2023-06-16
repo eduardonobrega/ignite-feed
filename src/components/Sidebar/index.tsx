3ab7b2c437167132b72c29b1a6c9fde74dad1df1
eduardo <eduardo.nobrega1999@hotmail.com>
2023-06-16 09:46:04 -0300

    add user type

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,22 +1,21 @@
 import { Profile } from '../Profile'
 import { PencilSimpleLine } from '@phosphor-icons/react'
 import styles from './styles.module.css'
+import { User } from '../Post'
 
 export function Sidebar() {
+  const user: User = {
+    name: 'Mayk Brito',
+    avatarUrl: 'https://github.com/maykbrito.png',
+    occupation: 'teacher in @Rocketseat',
+  }
   return (
     <aside className={styles.sidebar}>
       <img
         src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
         alt=""
       />
-      <Profile
-        user={{
-          name: 'Eduardo Nóbrega',
-          avatarUrl: 'https://github.com/eduardonobrega.png',
-          occupation: 'Web Developer',
-        }}
-        column
-      />
+      <Profile user={user} column />
 
       <footer>
         <a href="#">
