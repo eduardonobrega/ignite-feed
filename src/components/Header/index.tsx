ef555ba8d3ed556b4f0b0e9a7d0eeb9faa937efa
eduardo <eduardo.nobrega1999@hotmail.com>
2023-06-28 20:54:02 -0300

    button signOut

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,19 @@
 import styles from './styles.module.css'
 import igniteLogo from '../../assets/igniteLogo.svg'
+import { NavLink } from 'react-router-dom'
+import { Power } from '@phosphor-icons/react'
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={igniteLogo} alt="Logo do ignite" />
-      <h1>Ignite Feed</h1>
+      <div>
+        <img src={igniteLogo} alt="Logo do ignite" />
+        <h1>Ignite Feed</h1>
+      </div>
+
+      <NavLink to="/">
+        <Power />
+      </NavLink>
     </header>
   )
 }
