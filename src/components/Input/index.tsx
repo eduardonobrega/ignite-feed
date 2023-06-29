c882df9853ccb10b4193c19047ae019e2f300db6
eduardo <eduardo.nobrega1999@hotmail.com>
2023-06-28 21:46:15 -0300

    icon in input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,24 @@
+import { Icon } from '@phosphor-icons/react'
 import styles from './styles.module.css'
 import { InputHTMLAttributes } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string
+  label?: string
+  icon?: Icon
 }
 
-export function Input({ label, ...props }: InputProps) {
+export function Input({ label, icon: Icon, ...props }: InputProps) {
   return (
-    <div>
-      <label className={styles.label} htmlFor={label}>
-        {label}
-      </label>
-      <input {...props} className={styles.input} id={label} />
-    </div>
+    <>
+      {label && (
+        <label className={styles.label} htmlFor={label}>
+          {label}
+        </label>
+      )}
+      <div className={styles.wrapperInput}>
+        {Icon && <Icon />}
+        <input {...props} className={styles.input} id={label} />
+      </div>
+    </>
   )
 }
